01f6614b5a6eb51afccb9b441b7242a4407a55da
sierra OS <sierra@sierraui-macbook-air.local>
2019-01-29 02:48:56 +0900

    p.149

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by sierra OS on 2019. 1. 28..
+//  Copyright © 2019년 FunnyApp. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
