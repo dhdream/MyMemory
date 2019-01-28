9d0512ebe37acc3b35be8a968826d66cd255ed75
sierra OS <sierra@sierraui-macbook-air.local>
2019-01-29 02:49:06 +0900

    p.149

---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,18 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by sierra OS on 2019. 1. 28..
+//  Copyright © 2019년 FunnyApp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemoData {
+    var memoIdx : Int?
+    var title : String?
+    var contents : String?
+    var image : UIImage?
+    var regdate : Date?
+}
