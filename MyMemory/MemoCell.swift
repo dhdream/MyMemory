3d5d3d79a9754550704d41ccad62f5aded4b77fb
sierra OS <sierra@sierraui-macbook-air.local>
2019-01-31 00:46:42 +0900

    p.186

--- MyMemory/MemoCell.swift
+++ MyMemory/MemoCell.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class MemoCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    @IBOutlet var subject: UILabel!
+    @IBOutlet var contents: UILabel!
+    @IBOutlet var regdate: UILabel!
+    @IBOutlet var img: UIImageView!
+    
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
         // Initialization code
-    }
+//    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
         // Configure the view for the selected state
-    }
+//    }
 
 }
