8ae8319e2dd6f6a037c3cf26d8429d238e1b1c06
sierra OS <sierra@sierraui-macbook-air.local>
2019-01-31 00:46:08 +0900

    p.186

--- MyMemory/MemoListVC.swift
+++ MyMemory/MemoListVC.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class MemoListVC: UITableViewController {
-
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewWillAppear(_ animated: Bool) {
+      self.tableView.reloadData()
+    }
+    
+    /*
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,28 +30,47 @@ class MemoListVC: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    */
     // MARK: - Table view data source
-
+    /*
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
+    */
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        let count = self.appDelegate.memolist.count
+        return count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let row = self.appDelegate.memolist[indexPath.row]
+        let cellId = (row.image == nil ? "memoCell" : "memoCellWithImage")
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
 
-        // Configure the cell...
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.regdate?.text = formatter.string(from: row.regdate!)
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      let row = self.appDelegate.memolist[indexPath.row]
+        
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "MemoRead") as? MemoReadVC else {
+            return
+        }
+        
+        vc.param = row
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -91,5 +116,5 @@ class MemoListVC: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
+
